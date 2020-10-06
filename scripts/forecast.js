1a8e320e593db5409a4c1731141ec5037fe46ac4
Brandon U <unzueta.brandon@gmai.com>
2020-10-06 16:40:57 -0700

    creating getWeather api function

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,5 +1,17 @@
 const key = "Z3rZCNmlIJmMmrYQ5xE6xVZMxkbGxa2h";
 
+//WEATHER INFO ON CITY
+async function getWeather(id) {
+  const response = await fetch(
+    `http://dataservice.accuweather.com/currentconditions/v1/${id}?apikey=${key}`
+  );
+
+  const data = await response.json();
+
+  return data[0];
+}
+
+//GET CITY
 async function getCity(city) {
   const response = await fetch(
     `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${key}&q=${city}`
@@ -7,7 +19,5 @@ async function getCity(city) {
 
   const data = await response.json();
 
-  console.log(data[0]);
+  return data[0];
 }
-
-getCity("Los Angeles").then();
