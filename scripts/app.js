ea93d16dd16368272864f81f620c281ba1bc45be
Brandon U <unzueta.brandon@gmai.com>
2020-10-06 17:24:47 -0700

    updateUI functionality

---
+++ scripts/app.js
@@ -0,0 +1,40 @@
+const cityForm = document.getElementById("city-form");
+const card = document.getElementById("card");
+const details = document.getElementById("details");
+
+function updateUI(data) {
+  const { cityDetails, weather } = data;
+
+  details.innerHTML = `
+    <h5 class="my-3">${cityDetails.EnglishName}</h5>
+            <div class="my-3">${weather.WeatherText}</div>
+            <div class="display-4 my-4">
+              <span>${weather.Temperature.Imperial.Value}</span>
+              <span>&deg;F</span>
+    `;
+
+  //REMOVE d-none CLASS
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none");
+  }
+}
+
+async function updateCity(city) {
+  const cityDetails = await getCity(city);
+  const weather = await getWeather(cityDetails.Key);
+
+  return { cityDetails, weather };
+}
+
+cityForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const city = cityForm.city.value.trim();
+  cityForm.reset();
+
+  updateCity(city)
+    .then((data) => {
+      updateUI(data);
+    })
+    .catch((err) => console.log(err));
+});
